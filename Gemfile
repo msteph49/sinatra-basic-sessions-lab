d1a1e36f53cee8ad82ad517dbca754a067662287
Michael Stephens <mdstephens26@yahoo.com>
2021-02-28 13:37:53 -0500

    commit tree cb47cde05b5547b8bde5fd29b44a43beea14f509

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@
 source "https://rubygems.org"
 
 # gem "rails"
-
+require 'securerandom'
 gem 'sinatra'
     gem 'rake'
     gem 'thin'
--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    configure do 
+        set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+    end
+    get '/' do
+        erb :index
+    end
+    post '/checkout' do
+        session[:item] = params[:item]
+    end
 end
